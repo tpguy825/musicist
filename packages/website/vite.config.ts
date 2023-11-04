d46c65369bac70b080a304c9ab81c88e83e3e706
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2023-11-04 14:23:14 +0000

    fixed vite problem

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -4,5 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	plugins: [preact(), ssr({ prerender: true })],
+	root: ".",
 });
