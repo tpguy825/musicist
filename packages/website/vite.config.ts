57525d24b69eb2a3f3ab32e3b233e43f22aa032d
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2023-11-04 14:34:24 +0000

    just in case

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -5,6 +5,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	plugins: [preact(), ssr({ prerender: true })],
+	plugins: [preact(), ssr({ prerender: true, baseAssets: "." })],
 	base: ".",
 });
