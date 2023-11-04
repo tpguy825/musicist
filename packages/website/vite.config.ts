3286a97884cd88fbe7c2628358341788f8df8375
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2023-11-04 14:51:27 +0000

    removed broken bits

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -5,6 +5,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	plugins: [preact(), ssr({ prerender: true, baseAssets: "." })],
-	base: ".",
+	plugins: [preact(), ssr({ prerender: true })],
 });
